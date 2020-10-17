83e6f6bc224766e3efdf2cc6d891ac3512bdda2c
Nico De Witte <nico.dewitte@vives.be>
2020-10-17 08:58:08 +0200

    Add code

---
+++ CircleAppGroup2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CircleAppGroup2
+{
+    class Program
+    {
+        public static double RequestRadius()
+        {
+            Console.Write("Please enter the circle radius: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double RequestHeight()
+        {
+            Console.Write("Please enter the height of the cilinder: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public static void OutputResult(double radius, double area, double volume)
+        {
+            Console.WriteLine("For a cilinder with a radius of "
+                + radius + " the area is " + area + " and the volume is "
+                + volume);
+        }
+
+        public static double CalculateVolume(double radius, double height)
+        {
+            return CalculateArea(radius) * height;
+        }
+
+        static void Main(string[] args)
+        {
+            // Step 1 - Request Radius
+            double radius = RequestRadius();
+
+            // Step 2 - Request height
+            double height = RequestHeight();
+
+            // Step 3 - Calculate Area
+            double area = CalculateArea(radius);
+
+            // Step 4 - Calculate Cilinder Volume
+            double volume = CalculateVolume(radius, height);
+
+            // CalculateVolume(area, height); ?
+            //      << Verplicht gebruiker eerst CalculateArea() aan te roepen
+            // OF:
+            // CalculateVolume(radius, height); ? <<== VOORKEUR
+
+            // Step 5 - Show result to user
+            OutputResult(radius, area, volume);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Circle / Cilinder App - Group 2
+
+Demo code of chapter 7 - Methods. Calculating area of circle and volume of Cilinder.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/CircleAppGroup2.git
+```
